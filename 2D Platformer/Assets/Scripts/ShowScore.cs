88778b0b518b930e8f855b7fb5bb742d56557706
Opang33 <111104358+Opang33@users.noreply.github.com>
2022-10-21 13:37:09 +0700

    2D_Platformer-21_Oktober_2022
    Naufal Afif M - 02120147299

---
+++ 2D Platformer/Assets/Scripts/ShowScore.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowScore : MonoBehaviour
+{
+    
+    void FixedUpdate()
+    {
+        GetComponent<Text>().text = Data.score.ToString("000");
+    }
+}
